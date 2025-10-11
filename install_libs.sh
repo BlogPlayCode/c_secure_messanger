9849612b66a214bbc9814adddce88b7f298acdea
BlogPlayCode <blogplay1@gmail.com>
2025-10-11 11:44:05 +0300

    v0.4.5 install_libs.sh fix

--- install_libs.sh
+++ install_libs.sh
@@ -1,2 +1,15 @@
+rm -rf kyber
+rm -rf mbedtls
+
 git clone https://github.com/pq-crystals/kyber.git
 git clone https://github.com/Mbed-TLS/mbedtls.git
+
+cd mbedtls
+git checkout v3.6.1
+git submodule update --init --recursive
+
+mkdir build
+cd build
+sudo apt install cmake -y
+cmake ..
+cmake --build .
